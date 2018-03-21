5ab8c288dfc7831e4c8903a16a0cef35efb070ac
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-21 16:10:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      params[:pirate].each{|p| Pirate.new(p[:name], p[:weight], p[:height])}
+
+      params[:pirate][:ships].each{|s| Ship.new(s[:name], s[:type], s[:booty])}
+
+      erb :show
+    end
 
   end
 end
